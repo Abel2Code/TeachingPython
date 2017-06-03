d746915480cb8536e37773f3cd1b41b98ae94472
Abel2Code <asalinaswc1@aol.com>
2017-06-03 16:24:53 -0700

    Finished Game

--- Examples/Text-BasedGames/MultipleCharacterFighter.py
+++ Examples/Text-BasedGames/MultipleCharacterFighter.py
@@ -1,90 +1,159 @@
 import random  # Will be used to generate random numbers for attack
 
 
-def executeMarioTurn(marioHealth, sonicHealth):
-    validInput = False  # We will use this variable to decide whether to keep trying to get the user input
+class Player():  # Tracks players and information about them
+    def __init__(self, number, character):  # Class tracks there number and character - One could track usernames here
+        self.number = number
+        self.character = character  # stores object of type Character created below
 
-    while not validInput:  # This will run until the user enter a valid move
-        decision = input("Type attack for Mario to attack! \nType heal for Mario to heal!\n")
-        if decision == "attack" or decision == "heal":
-            validInput = True  # Stops loop if valid move
 
-    if decision == "attack":
-        attackValue = random.randint(marioAttackRange[0], marioAttackRange[1])  # Generates random value in attk range
-        sonicHealth -= attackValue  # Subtracts attackValue from current SonicHealth
-        print("Mario attacked Sonic and dealt", attackValue, "hp!")
+class Character():  # Stores attributes specific to each character Ex: Mario and Sonic
+    def __init__(self, name, maxHP, moves):
+        self.name = name
+        self.maxHP = maxHP  # Since a character cannot heal over their max health, we keep track of this value here
+        self.currentHP = maxHP
+        self.moves = moves  # For the sake of this example, we will assume Characters have two moves
+
+
+class Move():  # Stores information about Character's moevs
+    def __init__(self, damageMax, damageMin, healMax, healMin, itterates, description):
+        self.damageMax = damageMax
+        self.damageMin = damageMin
+        self.healMax = healMax
+        self.healMin = healMin
+        self.itterates = itterates
+        self.description = description
+
+    def use(self, user, opponent):  # Based on damage and heal values, this executes the move
+        for counter in range(1, self.itterates + 1):  # If a move itterates multiple times, this loop keeps track
+            if self.healMax > 0:  # Checks if move heals
+                user.currentHP += random.randint(self.healMin, self.healMax)  # Adds health to Character
+                if user.currentHP > user.maxHP:  # Checks if Character is over max health
+                    user.currentHP = user.maxHP
+                print(user.name, "healed!")
+
+            if self.damageMax > 0:  # Checks if the move deals damage
+                attackValue = random.randint(self.damageMin, self.damageMax)
+                opponent.currentHP -= attackValue
+                print(user.name, "attacked", opponent.name, "and dealt", attackValue, "hp!")
+
+
+def validMoveInput(input):
+    if input == "1" or input == "2":  # Since user input is always a string, we use "1" instead of 1
+        return True
     else:
-        marioHealth += marioHeal  # Adds marioHeal to current marioHealth
-        if marioHealth > 64:  # Makes sure Mario does not heal over max hp
-            marioHealth = 64
-        print("Mario healed!")
-    return [marioHealth, sonicHealth]  # Returns new hp values
+        return False
 
 
-def executeSonicTurn(marioHealth, sonicHealth):
-    validInput = False  # We will use this variable to decide whether to keep trying to get the user input
+def executeTurn(char1, char2):  # Logic for Character turn
+    question = "Would you like " + char1.name + " to " + char1.moves[0].description + " or " + \
+               char1.moves[1].description + "?\nType 1 for " + char1.moves[0].description + " and 2 for " + \
+               char1.moves[1].description + "\n"
 
-    while not validInput:  # This will run until the user enter a valid move
-        decision = input(
-            "Type heavy for Sonic to use his heavy attack! \nType fury for Sonic to use his fury attack!\n")
-        if decision == "heavy" or decision == "fury":
-            validInput = True  # Stops loop if valid move
-
-    if decision == "heavy":
-
-        # Generates random value in attk range
-        attackValue = random.randint(sonicMainAttackRange[0], sonicMainAttackRange[1])
-        marioHealth -= attackValue
-        print("Sonic attacked Mario and dealt", attackValue, "hp!")
-    else:
-        for counter in range(1, 4):  # Sonic fury attacks 3 times
-
-            # Generates random value in attk range
-            attackValue = random.randint(sonicQuickAttackRange[0], sonicQuickAttackRange[1])
-            marioHealth -= attackValue
-            print("On punch #" + str(counter) + ", sonic dealt " + str(attackValue) + " damage!")
-    return [marioHealth, sonicHealth]  # Returns new hp values
+    decision = input(question)
+    validDecision = validMoveInput(decision)
+    while not (validDecision):  # Checks if user inputted 1 or 2
+        print("Try again")
+        decision = input(question)
+        validDecision = validMoveInput(decision)
+    char1.moves[int(decision) - 1].use(char1, char2)  # executes move
 
 
-# Mario Stats
-marioHealth = 64  # Is the starting health that Mario starts with
+def initializeCharacters():  # Creates the characters. If we wanted to add a new Character, we would add it here
+    # Mario and Sonic are initialized in different methods so that our code is cleaner and easier to read
+    mario = initMario()
+    sonic = initSonic()
 
-marioAttackRange = [6, 8]  # Since this attack value is based on chance, we opt to use a list to hold both values
-marioHeal = 9  # Since the heal is not based on chance, we use an integer instead of a list
+    return [mario, sonic]  # Returns list of all characters
 
-# Sonic Stats
-sonicHealth = 44  # Is the starting health that Sonic starts with
 
-sonicMainAttackRange = [7, 10]  # Since this attack value is based on chance, we opt to use a list to hold both values
-sonicQuickAttackRange = [2, 5]  # Since this attack value is based on chance, we opt to use a list to hold both values
+def initMario():  # Creates Mario Character
+    moves = [Move(8, 6, 0, 0, 1, "attack"), Move(0, 0, 9, 9, 1, "heal")]  # Creates Mario move list
+    marioHealth = 64  # Is Mario's starting health
 
-# If we wish to change any of these values later, we just need to tweak the value!
+    return Character("Mario", marioHealth, moves)
 
-isMarioTurn = True  # Will keep track of which player's turn it is
+
+def initSonic():  # Creates Sonic Character
+    moves = [Move(10, 7, 0, 0, 1, "heavy attack"), Move(5, 2, 0, 0, 3, "fury attack")]  # Creates Sonic move list
+    sonicHealth = 44  # Is Sonic's starting health
+
+    return Character("Sonic", sonicHealth, moves)
+
+
+def listCharacters(characters):  # lists all initialized Characters
+    for counter in characters:
+        print(counter.name)
+
+
+def validCharacter(string, list):  # Checks if inputted is in the list passed into the method
+    for interval in list:
+        if interval.name == string:
+            return list.index(interval)
+    return -1  # If we cannot find the string, we return -1
+
+
+characters = initializeCharacters()  # Creates the character list
+print("Welcome!\n")
+print("These are the available characters:")
+listCharacters(characters)
+
+player1Character = (input("\nPlayer 1, choose a character!\n"))
+player1CharacterPos = validCharacter(player1Character, characters)
+
+while player1CharacterPos is -1:  # Checks if Player 1 inputted a valid Character name - Case Sensitive
+    print("Please try again")
+    player1Character = (input("Player 1, choose a character!"))
+    player1CharacterPos = validCharacter(player1Character, characters)
+
+player1Character = characters[player1CharacterPos]
+
+player1 = Player(1, player1Character)  # Creates Player 1
+
+characters.remove(characters[player1CharacterPos])
+
+print("\nThese are the available characters:")
+listCharacters(characters)
+
+player2Character = (input("\nPlayer 2, choose a character!\n"))
+player2CharacterPos = validCharacter(player2Character, characters)
+
+while player1CharacterPos is -1:  # Checks if Player 2 inputted a valid Character name - Case Sensitive
+    print("Please try again")
+    player2Character = (input("Player 2, choose a character!\n"))
+    player2CharacterPos = validCharacter(player2Character, characters)
+
+player2Character = characters[player2CharacterPos]
+
+player2 = Player(2, player2Character)
+
+isPlayer1Turn = True  # Will keep track of which player's turn it is
 # Since there are only 2 players, we can use a boolean
 
-while not (marioHealth <= 0 or sonicHealth <= 0):  # Checks to see that no character is dead
-    print("\nMario has", marioHealth, "hp!")
-    print("Sonic has", sonicHealth, "hp!\n")
-    if isMarioTurn:  # This conditional statement check's whos turn it is
-        print("Mario's Turn!")
-
-        # See Mario turn logic on line 3
-        tempHealth = executeMarioTurn(marioHealth, sonicHealth)  # We assign the list this method returns to a variable
-        marioHealth = tempHealth[0]  # executeMarioTurn returns a list with Mario and Sonic's health
-        sonicHealth = tempHealth[1]
-        isMarioTurn = False  # Makes it Sonic's turn
+# While loop checks to see that no character is dead
+while not (player1.character.currentHP <= 0 or player2.character.currentHP <= 0):
+    print("\n" + player1.character.name + " has " + str(player1.character.currentHP) + " hp!")
+    print(player2.character.name, "has", player2.character.currentHP, "hp!\n")
+    if isPlayer1Turn:  # This conditional statement check's whos turn it is
+        print(player1.character.name, "'s Turn!")
+        executeTurn(player1.character, player2.character)  # Executes Player's turn
+        isPlayer1Turn = False  # Makes it Player2's turn
     else:
-        print("Sonic's Turn")
-
-        # See Sonic turn logic on line
-        tempHealth = executeSonicTurn(marioHealth, sonicHealth)  # We assign the list this method returns to a variable
-        marioHealth = tempHealth[0]  # executeSonicTurn returns a list with Mario and Sonic's health
-        sonicHealth = tempHealth[1]
-        isMarioTurn = True  # Makes it Mario's Turn
+        print(player2.character.name, "'s Turn")
+        executeTurn(player2.character, player1.character)  # Executes Player's turn
+        isPlayer1Turn = True  # Makes it Player1's Turn
 
 # Get here once one player has dropped below 1 health
-if (marioHealth <= 0):
-    print("Mario has fainted. Sonic Wins at " + str(sonicHealth) + " hp!")
+if (player1.character.currentHP <= 0):
+    print(player1.character.name +
+          " has fainted. " +
+          player2.character.name +
+          " wins at " + str(player2.character.currentHP) +
+          " hp!")
 else:
-    print("Sonic has fainted. Mario Wins at " + str(marioHealth) + " hp!")
+    print(player2.character.name +
+          " has fainted. " +
+          player1.character.name +
+          " wins at " +
+          str(player1.character.currentHP) +
+          " hp!")
--- Examples/Text-BasedGames/UncommentedVersions/MultipleCharacterFighter.py
+++ Examples/Text-BasedGames/UncommentedVersions/MultipleCharacterFighter.py
@@ -1,78 +1,148 @@
 import random
 
+class Player():
+    def __init__(self,number,character):
+        self.number = number
+        self.character = character
 
-def executeMarioTurn(marioHealth, sonicHealth):
-    validInput = False
+class Character():
+    def __init__(self,name,maxHP,moves):
+        self.name = name
+        self.maxHP = maxHP
+        self.currentHP = maxHP
+        self.moves = moves
 
-    while not validInput:
-        decision = input("Type attack for Mario to attack! \nType heal for Mario to heal!\n")
-        if decision == "attack" or decision == "heal":
-            validInput = True
+class Move():
+    def __init__(self,damageMax,damageMin,healMax,healMin,itterates,description):
+        self.damageMax = damageMax
+        self.damageMin = damageMin
+        self.healMax = healMax
+        self.healMin = healMin
+        self.itterates = itterates
+        self.description = description
 
-    if decision == "attack":
-        attackValue = random.randint(marioAttackRange[0], marioAttackRange[1])
-        sonicHealth -= attackValue
-        print("Mario attacked Sonic and dealt", attackValue, "hp!")
+    def use(self,user,opponent):
+        for counter in range(1,self.itterates+1):
+            if self.healMax > 0:
+                user.currentHP +=  random.randint(self.healMin, self.healMax)
+                if user.currentHP > user.maxHP:
+                    user.currentHP = user.maxHP
+                print(user.name,"healed!")
+
+            if self.damageMax > 0:
+                attackValue = random.randint(self.damageMin,self.damageMax)
+                opponent.currentHP -= attackValue
+                print(user.name,"attacked",opponent.name,"and dealt",attackValue,"hp!")
+
+
+def validMoveInput(input):
+    if input == "1" or input == "2":
+        return True
     else:
-        marioHealth += marioHeal
-        if marioHealth > 64:
-            marioHealth = 64
-        print("Mario healed!")
-    return [marioHealth, sonicHealth]
+        return False
+
+def executeTurn(char1,char2):
+    question = "Would you like " + char1.name + " to " + char1.moves[0].description + " or " +\
+               char1.moves[1].description + "?\nType 1 for " + char1.moves[0].description + " and 2 for " +\
+               char1.moves[1].description + "\n"
+    decision = input(question)
+    validDecision = validMoveInput(decision)
+    while not (validDecision):
+        print("Try again")
+        decision = input(question)
+        validDecision = validMoveInput(decision)
+    char1.moves[int(decision) - 1].use(char1,char2)
+
+def initializeCharacters():
+    mario = initMario()
+    sonic = initSonic()
+
+    return[mario,sonic]
+
+def initMario():
+    moves = [Move(8,6,0,0,1,"attack"),Move(0,0,9,9,1,"heal")]
+    marioHealth = 64
+
+    return Character("Mario",marioHealth,moves)
+
+def initSonic():
+    moves = [Move(10,7,0,0,1,"heavy attack"),Move(5,2,0,0,3,"fury attack")]
+    sonicHealth = 44
+
+    return Character("Sonic",sonicHealth,moves)
 
 
-def executeSonicTurn(marioHealth, sonicHealth):
-    validInput = False
+def listCharacters(characters):
+    for counter in characters:
+        print(counter.name)
 
-    while not validInput:
-        decision = input(
-            "Type heavy for Sonic to use his heavy attack! \nType fury for Sonic to use his fury attack!\n")
-        if decision == "heavy" or decision == "fury":
-            validInput = True
+def validCharacter(string, list):
+    for interval in list:
+        if interval.name == string:
+            return list.index(interval)
+    return -1
 
-    if decision == "heavy":
-        attackValue = random.randint(sonicMainAttackRange[0], sonicMainAttackRange[1])
-        marioHealth -= attackValue
-        print("Sonic attacked Mario and dealt", attackValue, "hp!")
+characters = initializeCharacters()
+print("Welcome!\n")
+print("These are the available characters:")
+listCharacters(characters)
+
+player1Character = (input("\nPlayer 1, choose a character!\n"))
+player1CharacterPos = validCharacter(player1Character, characters)
+
+while player1CharacterPos is -1:
+    print("Please try again")
+    player1Character = (input("Player 1, choose a character!"))
+    player1CharacterPos = validCharacter(player1Character, characters)
+
+player1Character = characters[player1CharacterPos]
+
+player1 = Player(1,player1Character)
+
+characters.remove(characters[player1CharacterPos])
+
+print("\nThese are the available characters:")
+listCharacters(characters)
+
+player2Character = (input("\nPlayer 2, choose a character!\n"))
+player2CharacterPos = validCharacter(player2Character, characters)
+
+while validCharacter(player2Character, characters) is -1:
+    print("Please try again")
+    player2Character = (input("Player 2, choose a character!\n"))
+    player2CharacterPos = validCharacter(player2Character, characters)
+
+player2Character = characters[player2CharacterPos]
+
+player2 = Player(2,player2Character)
+
+isPlayer1Turn = True
+
+while not (player1.character.currentHP <= 0 or player2.character.currentHP <= 0):
+    print("\n" + player1.character.name + " has " + str(player1.character.currentHP) +" hp!")
+    print(player2.character.name,"has",player2.character.currentHP,"hp!\n")
+    if isPlayer1Turn:
+        print(player1.character.name,"'s Turn!")
+
+        executeTurn(player1.character,player2.character)
+        isPlayer1Turn = False
     else:
-        for counter in range(1, 4):
-            attackValue = random.randint(sonicQuickAttackRange[0], sonicQuickAttackRange[1])
-            marioHealth -= attackValue
-            print("On punch #" + str(counter) + ", sonic dealt " + str(attackValue) + " damage!")
-    return [marioHealth, sonicHealth]
+        print(player2.character.name,"'s Turn")
 
-marioHealth = 64
+        executeTurn(player2.character,player1.character)
+        isPlayer1Turn = True
 
-marioAttackRange = [6, 8]
-marioHeal = 9
-
-
-sonicHealth = 44
-
-sonicMainAttackRange = [7, 10]
-sonicQuickAttackRange = [2, 5]
-
-isMarioTurn = True
-
-while not (marioHealth <= 0 or sonicHealth <= 0):
-    print("\nMario has", marioHealth, "hp!")
-    print("Sonic has", sonicHealth, "hp!\n")
-    if isMarioTurn:
-        print("Mario's Turn!")
-
-        tempHealth = executeMarioTurn(marioHealth, sonicHealth)
-        marioHealth = tempHealth[0]
-        sonicHealth = tempHealth[1]
-        isMarioTurn = False
-    else:
-        print("Sonic's Turn")
-
-        tempHealth = executeSonicTurn(marioHealth, sonicHealth)
-        marioHealth = tempHealth[0]
-        sonicHealth = tempHealth[1]
-        isMarioTurn = True
-
-if (marioHealth <= 0):
-    print("Mario has fainted. Sonic Wins at " + str(sonicHealth) + " hp!")
+if (player1.character.currentHP <= 0):
+    print(player1.character.name +
+          " has fainted. " +
+          player2.character.name +
+          " wins at " + str(player2.character.currentHP) +
+          " hp!")
 else:
-    print("Sonic has fainted. Mario Wins at " + str(marioHealth) + " hp!")
+
+    print(player2.character.name +
+          " has fainted. " +
+          player1.character.name +
+          " wins at " +
+          str(player1.character.currentHP) +
+          " hp!")
